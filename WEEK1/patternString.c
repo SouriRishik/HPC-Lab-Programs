4828d0589a73cb41d7e4d00c2f5a37ad5bc3eb95
Souri Rishik <souri.rishik27@gmail.com>
2025-07-29 09:42:16 +0530

    fix: changed logic

--- WEEK1/patternString.c
+++ WEEK1/patternString.c
@@ -6,16 +6,25 @@ int main() {
     char input_word[100];
     printf("Enter a word: ");
     scanf("%s", input_word);
+
     int n = strlen(input_word);
-    printf("Output: ");
-    #pragma omp parallel for
-    for (int i = 0; i < n; i++) {
-        int count = i + 1;
-        for (int j = 0; j < count; j++) {
-            printf("%c", input_word[i]);
+    char output[1000] = "";
+
+    #pragma omp parallel
+    {
+        char local[100] = "";
+        #pragma omp for nowait
+        for (int i = 0; i < n; i++) {
+            int count = i + 1;
+            for (int j = 0; j < count; j++) {
+                local[j] = input_word[i];
+            }
+            local[count] = '\0';
+            #pragma omp critical
+            strcat(output, local);
         }
     }
-    printf("\n");
+
+    printf("Output: %s\n", output);
     return 0;
 }
-
